30968604147e7643611c3f252dc448e2f1267b74
vinayshashank <vinay9023@gmail.com>
2016-02-03 18:44:38 -0500

    added comments

--- coding interview.py
+++ coding interview.py
@@ -3,7 +3,7 @@ import math
 a,b = map(int,raw_input().strip().split(" "))
 fact = math.factorial
 print fact(a)/fact(b)/fact(a-b)
---------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 result=[]
@@ -20,7 +20,7 @@ for i in s2:
         result.append(i)
 
 print " ".join(result)
---------------------------------
+#--------------------------------
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 a,b=map(int,raw_input().strip().split(" "))
 
@@ -30,7 +30,7 @@ for i in range(b):
     else:
         a=3*a+1
 print a
----------------------------------
+#---------------------------------
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 #import numpy as np
 import math
@@ -47,4 +47,4 @@ z = sum(c[i]*v[i] for i in range(len(c)))
 logit = 1.0/(1.0 + math.exp(-z))
 
 print '%.3f' % (logit)
------------------------------------
+#-----------------------------------
